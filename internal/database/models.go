6f31757d5f232534ec4a554e48b327f5ca668b5d
Hasan <imhasandl04@gmail.com>
2024-11-12 03:02:54 +0500

    add posts into the databse

--- internal/database/models.go
+++ internal/database/models.go
@@ -5,26 +5,39 @@
 package database
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Feed struct {
-	ID        uuid.UUID
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Name      string
-	Url       string
-	UserID    uuid.UUID
+	ID            uuid.UUID
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Name          string
+	Url           string
+	UserID        uuid.UUID
+	LastFetchedAt sql.NullTime
 }
 
 type FeedFollow struct {
-	ID 		 uuid.UUID
+	ID        uuid.UUID
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	UserID 	 uuid.UUID
-	FeedID 	 uuid.UUID
+	UserID    uuid.UUID
+	FeedID    uuid.UUID
+}
+
+type Post struct {
+	ID          uuid.UUID
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	Title       string
+	Url         string
+	Description sql.NullString
+	PublishedAt sql.NullTime
+	FeedID      uuid.UUID
 }
 
 type User struct {
@@ -33,5 +46,3 @@ type User struct {
 	UpdatedAt time.Time
 	Name      string
 }
-
-
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/imhasandl/blog-aggregator/internal/config"
 	"github.com/imhasandl/blog-aggregator/internal/database"
+
 	_ "github.com/lib/pq"
 )
 
@@ -49,6 +50,7 @@ func main() {
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollows))
 	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
+	cmds.register("browse", middlewareLoggedIn(handlerBrowse))
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: cli <command> [args...]")
